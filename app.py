faf777f591d722b34da96345e90590ffa2e9c3bb
Chetan Badhe <csbadhe08@gmail.com>
2018-01-02 02:01:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,6 @@ def processRequest(req):
         return {}
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
-    
     if yql_query is None:
         return {}
         yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
@@ -65,7 +64,6 @@ def processRequest(req):
         baseurl = "https://bitbns.com/order/getTicker"
         result = urlopen(baseurl).read()
         data = json.loads(result)
-        main_data=data['']
         res = makeWebhookResultForGetJoke(data)
     else:
         return {}
@@ -82,28 +80,6 @@ def makeYqlQuery(req):
 
     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
-
-def processRequest(req):
-    if req.get("result").get("action")=="yahooWeatherForecast":
-        baseurl = "https://query.yahooapis.com/v1/public/yql?"
-        yql_query = makeYqlQuery(req)
-        if yql_query is None:
-           return {}
-        yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-        result = urlopen(yql_url).read()
-        data = json.loads(result)
-        res = makeWebhookResult(data)
-    elif req.get("result").get("action")=="getjoke":
-        baseurl = "https://bitbns.com/order/getTicker"
-        result = urlopen(baseurl).read()
-        data = json.loads(result)
-        main_data=data['buyPrice']
-        res = makeWebhookResultForGetJoke(main_data)
-    else:
-        return {}
- 
-    return res
-
 def makeWebhookResultForGetJoke(data):
     valueString = data.get('value')
     joke = valueString.get('joke')
@@ -116,6 +92,29 @@ def makeWebhookResultForGetJoke(data):
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
+
+def makeWebhookResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
 
     # print(json.dumps(item, indent=4))
 
